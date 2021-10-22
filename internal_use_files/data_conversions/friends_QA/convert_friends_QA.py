aaf2a5b632388f92c6e96f5eb9479c85fefe8d8c
Alon Albalak <alonalbalak@gmail.com>
2021-10-21 17:00:33 -0700

    add friends QA dataset

---
+++ internal_use_files/data_conversions/friends_QA/convert_friends_QA.py
@@ -0,0 +1,56 @@
+import os
+import json
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
+detokenizer = TreebankWordDetokenizer()
+
+def format_data(data):
+    formatted_data = {
+        "metadata":
+        {
+            "dataset_name":"friends_QA",
+            "tasks":[
+                "question_answering"
+            ]
+        },
+        "data":[]
+    }
+
+    for scene in data['data']:
+        dialogue = []
+        utterances = scene['paragraphs'][0]['utterances:']
+        for utt in utterances:
+            formatted_turn = {
+                "turn_id": utt['uid'],
+                "speakers":utt['speakers'],
+                "utterance":detokenizer.detokenize(utt['utterance'].split())
+            }
+            dialogue.append(formatted_turn)
+        qas = scene['paragraphs'][0]['qas']
+        for qa in qas:
+            formatted_datum = {
+                "dialogue_id":qa['id'],
+                "dialogue_metadata":{
+                    "question_answering":None
+                },
+                "dialogue":dialogue,
+                "question_answering":{
+                    "query":qa['question'],
+                    "answers":qa['answers']
+                }
+            }
+            formatted_data['data'].append(formatted_datum)
+    return formatted_data
+
+
+TLiDB_path="TLiDB_friends_QA"
+if not os.path.isdir(TLiDB_path):
+    os.mkdir(TLiDB_path)
+
+data_partitions = [["trn","train"],["dev","dev"],["tst","test"]]
+for p in data_partitions:
+    data_path=f"friendsqa_{p[0]}.json"
+    data = json.load(open(data_path,"r"))
+    formatted_data = format_data(data)
+    with open(os.path.join(TLiDB_path,f"friends_QA_{p[1]}.json"), "w") as f:
+        json.dump(formatted_data, f, indent=2)
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -3,6 +3,8 @@ import json
 from urllib.request import urlopen
 from io import BytesIO
 from zipfile import ZipFile
+import logging
+logger = logging.getLogger(__name__)
 
 # loads dataset into memory, caches them if they are not already on disk
 
@@ -14,11 +16,14 @@ dataset_urls = {
     "multiwoz_22": "https://drive.google.com/uc?export=download&id=1sqaiYTm9b9SPEzehdjp_DXEovVId6Fvq",
     "clinc150": "https://drive.google.com/uc?export=download&id=1Vu_1GjGF3bJo77Amw5qNU7z8Smn2B6Av",
     "friends_ER": "https://drive.google.com/uc?export=download&id=13b8PsYGShUDkoYS6m4YHbqWlxaOW3i_L",
-    "friends_RC": "https://drive.google.com/uc?export=download&id=1XnRnFm9J5ySARPEr7aW3JE1jMetdcwsl"
+    "friends_RC": "https://drive.google.com/uc?export=download&id=1QBgOV6YI8wuQpRAu0XPxzoM77ma7zA_o",
+    "friends_QA": "https://drive.google.com/uc?export=download&id=1rhR-l-9Zl9cUpCOSpHbsBedfBUmTRVbN"
 }
 
 def download_and_unzip(url, extract_to='.'):
+    logger.info(f"Waiting for response from {url}")
     http_response = urlopen(url)
+    logger.info("Downloading data from {url}")
     zipfile = ZipFile(BytesIO(http_response.read()))
     zipfile.extractall(path=extract_to)
 
@@ -32,6 +37,7 @@ def load_dataset(name):
     # download and unzip dataset if needed
     if name not in os.listdir("../datasets"):
         download_and_unzip(dataset_urls[name],"../datasets")
+        logger.info(f"Extracted files to /datasets/{name}")
 
     ds = load_dataset_local(name)
 
