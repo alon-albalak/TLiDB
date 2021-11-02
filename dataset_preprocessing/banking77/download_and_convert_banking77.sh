2bf0110a5309ad2527c663a155ab3d4e015becad
Gyuwan Kim <kgwmath@gmail.com>
2021-11-02 10:45:29 -0700

    add scripts for banking77 dataset

---
+++ dataset_preprocessing/banking77/download_and_convert_banking77.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+wget -O train.csv https://raw.githubusercontent.com/PolyAI-LDN/task-specific-datasets/master/banking_data/train.csv
+wget -O test.csv https://raw.githubusercontent.com/PolyAI-LDN/task-specific-datasets/master/banking_data/test.csv
+wget -O categories.json https://raw.githubusercontent.com/PolyAI-LDN/task-specific-datasets/master/banking_data/categories.json
+
+python3 convert_banking77.py
+
+rm train.csv
+rm categories.csv
