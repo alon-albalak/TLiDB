8dc93cba8bcaf718d2599ec09e8e655966106c57
Alon Albalak <alon_albalak@ucsb.edu>
2021-11-08 16:24:50 -0800

    add F1 metric

--- TLiDB/metrics/all_metrics.py
+++ TLiDB/metrics/all_metrics.py
@@ -1,4 +1,5 @@
-from .metrics import ElementwiseMetric
+from .metrics import Metric, ElementwiseMetric
+import sklearn.metrics
 
 def multiclass_logits_to_pred(logits):
     """
@@ -19,3 +20,33 @@ class Accuracy(ElementwiseMetric):
         if self.prediction_fn is not None:
             y_pred = self.prediction_fn(y_pred)
         return (y_pred==y_true).float()
+
+class F1(Metric):
+    def __init__(self, prediction_fn=None, name=None, average='macro'):
+        """
+        Calculate F1 score
+        Args:
+            - prediction_fn: Function to convert y_pred into the same format as y_true (for example, convert logits to max index)
+            - name (str): Name of the metric
+            - average (str): one of ['binary', 'micro', 'macro', 'weighted', 'samples']
+        For further documentation, see https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
+        """
+        self.prediction_fn = prediction_fn
+        self.average = average
+        if name is None:
+            name = 'F1'
+            if average is not None:
+                name += f'-{self.average}'
+        super.__init__(name=name)
+
+    def _compute(self, y_pred,y_true,labels=None):
+        """
+        Args:
+            - y_pred: Predicted labels
+            - y_true: Ground truth labels
+            - labels: The set of labels to include when average != 'binary'  (if None, will use all labels)
+        See https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html for further documentation
+        """
+        if self.prediction_fn is not None:
+            y_pred = self.prediction_fn(y_pred)
+        score = sklearn.metrics.f1_score(y_true, y_pred, average=self.average, labels=labels)
