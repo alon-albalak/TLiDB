635df4fc2c7db973541e19febbec1bb9c0782a0b
Alon Albalak <alonalbalak@gmail.com>
2021-12-11 13:37:31 -0800

    use same optimizer config for all models

--- examples/configs/model_config.py
+++ examples/configs/model_config.py
@@ -1,21 +1,21 @@
 t5_config = {
     "model": "t5-base",
-    "optimizer": "Adafactor",
+    "optimizer": "Adam",
     "learning_rate": 3e-5,
     "fp16": False,
-    "effective_batch_size":128
+    "effective_batch_size":60
 }
 gpt2_config = {
     "model": "gpt2",
     "optimizer": "Adam",
     "learning_rate": 3e-5,
     "fp16": True,
-    "effective_batch_size":128
+    "effective_batch_size":60
 }
 bert_config = {
     "model": "bert-base-uncased",
     "optimizer": "Adam",
     "learning_rate": 3e-5,
     "fp16": True,
-    "effective_batch_size":40
+    "effective_batch_size":60
 }
